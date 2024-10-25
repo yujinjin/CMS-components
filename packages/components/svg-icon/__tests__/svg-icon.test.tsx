ca48269db945a426e9d5af5ee65721fcfc166b40
jackyu <yujinjin9@126.com>
2024-10-25 10:32:16 +0800

    test(components): [svg-icon]完善测试用例      docs文档TS声明配置调整

--- packages/components/svg-icon/__tests__/svg-icon.test.tsx
+++ packages/components/svg-icon/__tests__/svg-icon.test.tsx
@@ -2,17 +2,23 @@
  * @创建者: yujinjin9@126.com
  * @创建时间: 2023-04-04 10:45:56
  * @最后修改作者: yujinjin9@126.com
- * @最后修改时间: 2023-04-04 14:43:25
+ * @最后修改时间: 2024-10-25 10:20:01
  * @项目的路径: \CMS-components\packages\components\svg-icon\__tests__\svg-icon.test.tsx
  * @描述: svg icon 测试用例
  */
 import { mount } from "@vue/test-utils";
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
 import SvgIcon from "../src/svg-icon.vue";
 
 describe("svg-icon.vue", () => {
-    it("render test & class", () => {
+    test("render test & class", () => {
         const wrapper = mount(() => <SvgIcon value="" />);
         expect(wrapper.find("i").classes()).toContain("icon");
+    });
+
+    test("value", () => {
+        const value = "open";
+        const wrapper = mount(() => <SvgIcon value={value} />);
+        expect(wrapper.find("i.icon > svg.svg-icon > use").element.getAttribute("xlink:href")).toEqual("#icon-" + value);
     });
 });
--- play/src/main.ts
+++ play/src/main.ts
@@ -7,7 +7,10 @@
  * @描述: 入口页
  */
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import App from "./example/index.vue";
 
 const app = createApp(App);
+app.use(ElementPlus, { locale: zhCn });
 app.mount("#app");
