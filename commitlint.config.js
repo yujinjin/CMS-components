eed2abf9cbe81a495dc31ba5f14c6f4a7ba30832
jackyu <yujinjin9@126.com>
2026-06-05 16:13:17 +0800

    chore(all): 更新scope枚举值和提交类型列表
    将原有的play scope改为examples，并新增revert、test到提交类型枚举中

--- commitlint.config.js
+++ commitlint.config.js
@@ -17,7 +17,7 @@ export default {
     // rule配置说明：rule由name和配置数组组成，如：'name:[0, 'always', 72]'，数组中第一位为level，可选0,1,2，0为disable，1为warning，2为error，第二位为应用与否，可选always|never，第三位该rule的值。
     rules: {
         // scope的枚举值
-        "scope-enum": [2, "always", ["components", "playground", "docs", "play", "all"]],
+        "scope-enum": [2, "always", ["components", "playground", "docs", "play", "examples", "all"]],
         // 改变预设中的提交类型
         "type-enum": [2, "always", ["feat", "ui", "fix", "docs", "style", "refactor", "build", "ci", "perf", "chore", "revert", "test"]],
         // body 以空行开始
